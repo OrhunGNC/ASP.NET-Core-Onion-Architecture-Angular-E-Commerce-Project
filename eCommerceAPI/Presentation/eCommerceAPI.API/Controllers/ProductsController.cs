48dd643c70158344f501b9fdee8fc878898d7a13
Orhun Cem Gença <orhungenca@hotmail.com>
2023-12-26 14:01:25 +0300

    Creating Base Onion Architecture
    Creating Base Onion Architecture

---
+++ eCommerceAPI/Presentation/eCommerceAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using eCommerceAPI.Application.Abstractions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public IActionResult GetProducts()
+        {
+            var result = _productService.GetProducts();
+            return Ok(result);
+        }
+    }
+}
